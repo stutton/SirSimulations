62b748e2f92d918e31fafd9644848744ceba21f3
Stephen Tutton <stephen.tutton@outlook.com>
2020-04-06 14:49:42 -0400

    adding text, inprogress

---
+++ src/MonoGame/SirSimulation/Models/ParametersText.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SirSimulation.Models
+{
+    public class ParametersText : Engine.IDrawable
+    {
+        public ParametersText(SpriteFont header, SpriteFont body, Vector2 position)
+        {
+            Header = header;
+            Body = body;
+            Position = position;
+        }
+
+        public SpriteFont Header { get; }
+        public SpriteFont Body { get; }
+        public Vector2 Position { get; }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawString(Header, "Parameters", Position, Color.White);
+
+        }
+    }
+}
